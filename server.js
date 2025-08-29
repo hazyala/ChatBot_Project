4feaacac81b1f42718cdbdf655ebe775bb6d7561
hazyala <heamin0603@naver.com>
2025-08-29 18:54:03 +0900

    로컬 다운로드 오류 수정

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import fetch from "node-fetch";
+const app = express();
 
 const PORT = process.env.PORT || 10000;
 
